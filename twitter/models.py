9f917f78831b3049cf88c164cdd87328155ac48f
galileoguzman <galileoguzman@gmail.com>
2019-10-05 16:23:17 -0500

    Admin for twit

--- twitter/models.py
+++ twitter/models.py
@@ -1,6 +1,34 @@
 from django.db import models
 
+from django.utils.safestring import mark_safe
+
 # Create your models here.
 class Twit(models.Model):
 	text = models.CharField(max_length=280)
 	image = models.ImageField('twits')
+
+	def __str__(self):
+		return self.text
+
+	def save(self, *args, **kwargs):
+		# Enviar twit
+		# Add python-twitter
+		# instance of python-twitter
+		# send twit with self.text		
+        super(Twit, self).save(*args, **kwargs)
+
+
+	def thumb_image(self):
+		return self.image.url
+
+
+	def thumbnail(self):
+		if self.image:
+			return mark_safe(
+				f'<image src="{self.image.url}" width=60 height=60 style="border-radius:50%" />'
+				)
+		else:
+			return u'No image file found'
+
+	thumbnail.short_description = 'Picture'
+	thumbnail.allow_tags = True
